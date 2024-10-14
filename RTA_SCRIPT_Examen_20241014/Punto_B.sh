09b32075748e200bc45981888a2b4dd1aca7d383
Lucas-montes03 <165548785+Lucas-montes03@users.noreply.github.com>
2024-10-13 23:38:08 -0300

    ADD: Punto_B resuelto

---
+++ RTA_SCRIPT_Examen_20241014/Punto_B.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Creando TXT para ingresar los comandos
+cat > comandos_fdisk.txt <<EOF
+
+n
+p
+
+
++1GB
+n
+e
+
+
+
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+n
+
++1GB
+w
+EOF
+#Creando particiones en disco
+sudo fdisk /dev/sdc < comandos_fdisk.txt
+#Formateando particiones
+sudo partprobe /dev/sdc
+sudo mkfs.ext4 /dev/sdc1
+sudo mkfs.ext4 /dev/sdc5
+sudo mkfs.ext4 /dev/sdc6
+sudo mkfs.ext4 /dev/sdc7
+sudo mkfs.ext4 /dev/sdc8
+sudo mkfs.ext4 /dev/sdc9
+sudo mkfs.ext4 /dev/sdc10
+sudo mkfs.ext4 /dev/sdc11
+sudo mkfs.ext4 /dev/sdc12
+sudo mkfs.ext4 /dev/sdc13
+#Cambio de fstab para forma persistente
+echo "/dev/sdc1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc5 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc6 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc7 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc8 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc9 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc10 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc11 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc12 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc13 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo monut -a
