398d8fac793b8fb935247f8bfef7dbf366d0f30c
Lucas-montes03 <mikelmineitor@gmail.com>
2024-10-15 02:15:05 +0000

    ADD: Agregando RTA_Archivos

--- RTA_SCRIPT_Examen_20241014/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241014/Punto_E.sh
@@ -2,18 +2,18 @@
 
 #Creando archivo txt
 
-touch $HOME/repogit/UTNFRA_SO_1P2C_2024_Montesdeoca/RTA_ARCHIVOS_Examen_20241011/Filtro_Basico.txt
+touch $HOME/repogit/UTNFRA_SO_1P2C_2024_MontesdeOca/RTA_ARCHIVOS_Examen_20241014/Filtro_Basico.txt
 
 echo "Se agregó el archivo 'Filtro_Basico.txt'"
 
 #Agregando información de la RAM al txt
 
-cat /proc/meminfo | head -n 1 > $HOME/repogit/UTNFRA_SO_1P2C_2024_Montesdeoca/RTA_ARCHIVOS_Examen_20241011/Filtro_Basico.txt
+cat /proc/meminfo | head -n 1 > $HOME/repogit/UTNFRA_SO_1P2C_2024_MontesdeOca/RTA_ARCHIVOS_Examen_20241014/Filtro_Basico.txt
 
 echo "Se agregó la información de la memoria RAM al archivo 'Filtro_Basico.txt'"
 
 #Agregando información del fabricante del chásis al txt
 
-sudo dmidecode -t chassis | head -n 7 | tail -n 2 >> $HOME/repogit/UTNFRA_SO_1P2C_2024_Montesdeoca/RTA_ARCHIVOS_Examen_20241011/Filtro_Basico.txt
+sudo dmidecode -t chassis | head -n 7 | tail -n 2 >> $HOME/repogit/UTNFRA_SO_1P2C_2024_MontesdeOca/RTA_ARCHIVOS_Examen_20241014/Filtro_Basico.txt
 
 echo "Se agregó al archivo 'Filtro_Basico.txt' la información del fabricante del chásis."
